9fb1b246eed529038e5ec5413619200dfb225938
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-04-05 01:42:38 +0200

    Antagonist Trone Room ready

--- Scripts/Segment.gd
+++ Scripts/Segment.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 export var valid_enter = { "U" :false, "D": false, "L":false, "R":false }
 export var is_cleaned  = false
 var gates_open   = true
@@ -12,7 +11,7 @@ func close_gates():
 	timer = 0.0
 	for child in $Gates.get_children():
 		child.close_gate()
-		
+
 func open_gates():
 	if gates_open: return
 	gates_open = true
@@ -23,7 +22,6 @@ func player_inside():
 	if is_cleaned : return false
 	var i = int(Util.player.position.x / Util.SEGMENT_SIZE.x)
 	var j = int(Util.player.position.y / Util.SEGMENT_SIZE.y)
-	
 	if Vector2(i,j) == int_position : return true
 	return false
 
@@ -36,7 +34,7 @@ func _process(delta):
 		timer += delta
 		if timer > open_after: is_cleaned = true
 	elif is_cleaned   : open_gates() 
-		
+
 
 #JUNK_USUED_CODE
 #onready var placeholder_tile_set_id = {
@@ -50,13 +48,13 @@ func _process(delta):
 #	$TileMap.tile_set.find_tile_by_name("H") : "H",
 #	$TileMap.tile_set.find_tile_by_name("I") : "I"
 #}
-#
+
 func generate_enemies():
 	for child in $EnemiesMarker.get_children():
 		var instance = Util.get_enemy_instance()
 		instance.position = child.position
 		$EnemiesMarker.add_child(instance)
-#
+
 func _ready():
 #	print(placeholder_tile_set_id)
 #	print($TileMap.tile_set.get_tiles_ids())
@@ -73,7 +71,7 @@ func _ready():
 #	var size  = $TileMap.get_used_rect().size 
 #	var p     = $TileMap.get_used_rect().position
 #	print($TileMap.tile_set.get_tiles_ids())
-##	print(placeholder_tile_set_id)
+#	print(placeholder_tile_set_id)
 #	#print(tilest_info.get_tiles_ids())
 #
 #	var sss = ""
@@ -98,5 +96,5 @@ func _ready():
 #			elif cell_id != -1 : print( cell_id )
 #	$TileMap.tile_set = new_tileset
 #		#sss += "\n"
-##	print( sss)
-#
+#	print( sss)
+
