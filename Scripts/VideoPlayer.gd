57f499fc2b2cd3665d44fc7310f1ee3d3ac35cfe
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-04-06 01:31:29 +0200

    Update VideoPlayer.gd

--- Scripts/VideoPlayer.gd
+++ Scripts/VideoPlayer.gd
@@ -7,4 +7,9 @@ func _on_VideoPlayer_finished():
 
 func _on_Control_finished():
 	get_tree().change_scene("res://Scenes/Game.tscn")
-	pass # Replace with function body.
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene("res://Scenes/Game.tscn")
+
